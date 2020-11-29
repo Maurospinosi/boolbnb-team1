cea9733238d6a3b08b93e9cd8b899eb5fd04654e
Davide Preti <davide.preti86@gmail.com>
2020-11-29 13:44:38 +0100

    ricerca nell'header funzionante

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,10 +4,22 @@
     <img src="{{asset('../img/logo.png')}}" alt="Logo">
 
     {{-- Barra di ricerca --}}
-    <div class="search-container header-search">
-        <input type="search" placeholder="Search...">
-        <span><i class="fas fa-search" ></i></span>
-    </div>
+    <form action="{{route('guest/search/results')}}" method="POST">
+        @csrf
+        @method('POST')
+
+        <div class="search-container header-search">
+            <input type="search" id="form-address" placeholder="Search...">
+            <input hidden type="text" class="form-control" id="form-address2" placeholder="Regione" name="region">
+            <input hidden type="text" class="form-control" name="zipcode" id="form-zip" placeholder="CAP">
+            <input hidden type="text" class="form-control" name="city" id="form-city" placeholder="Città">
+            <input hidden type="text" class="form-control" name="country" id="form-country" placeholder="Nazione">
+            <input hidden type="text" class="form-control" name="lat" id="form-lat"/>
+            <input hidden type="text" class="form-control" name="lon" id="form-lng"/>
+            <input hidden type="submit">
+            <span><i class="fas fa-search" ></i></span>
+        </div>
+    </form>
 
     {{-- Menu/Hamburger --}}
     <ul>
