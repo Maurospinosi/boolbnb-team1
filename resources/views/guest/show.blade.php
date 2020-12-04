6f8577869a709a4c99c38e1d882eda2b1c36dd13
Margherita <margherita.testi@gmail.com>
2020-12-05 00:11:29 +0100

    continuo modifica show

--- resources/views/guest/show.blade.php
+++ resources/views/guest/show.blade.php
@@ -94,7 +94,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col">
+            <div class="col-md-12">
                 <div class="d-flex flex-column bd-highlight mb-3">
                     <div class="p-2 bd-highlight">
                         <h6>Servizi</h6>
@@ -110,41 +110,44 @@
         </div>
         
 
-        <div class="right ">
-            <div class="form-box show-sticky">
-                <div class="card-body">
-                    <h3>Contatta il proprietario</h3>
-                    <form action="{{route('guest/message.store')}}" method="post">
-                        @csrf
-                        @method('POST')
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Nome</label>
-                            <input type="name" class="form-control " name="name" value="{{(Auth::user()) ? Auth::user()->name : ''}}" required maxlength="90" {{(Auth::user()) ? "readonly='readonly'" : ''}} id="exampleInputEmail1"
-                              aria-describedby="emailHelp">
-                            <label for="exampleInputEmail1">Email</label>
-                            <input type="email" class="form-control " name="email" value="{{(Auth::user()) ? Auth::user()->email : ''}}" required maxlength="90" {{(Auth::user()) ? "readonly='readonly'" : ''}} id="exampleInputEmail1"
-                              aria-describedby="emailHelp">
-                            <label for="message">Messaggio</label>
-                            <textarea class="form-control" name="message" required minlength="10" maxlength="700" id="message" cols="30" rows="10"></textarea>
-                        </div>
-                        <input type="hidden" name="house_id" value="{{$house->id}}">
-                        <button class="form__btn " type="submit">Invia</button>
-                    </form>
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="row ">
+            <div class="col-md-6">
+                <div class="form-box show-sticky">
+                    <div class="card-body">
+                        <h3>Contatta il proprietario</h3>
+                        <form action="{{route('guest/message.store')}}" method="post">
+                            @csrf
+                            @method('POST')
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Nome</label>
+                                <input type="name" class="form-control " name="name" value="{{(Auth::user()) ? Auth::user()->name : ''}}" required maxlength="90" {{(Auth::user()) ? "readonly='readonly'" : ''}} id="exampleInputEmail1"
+                                aria-describedby="emailHelp">
+                                <label for="exampleInputEmail1">Email</label>
+                                <input type="email" class="form-control " name="email" value="{{(Auth::user()) ? Auth::user()->email : ''}}" required maxlength="90" {{(Auth::user()) ? "readonly='readonly'" : ''}} id="exampleInputEmail1"
+                                aria-describedby="emailHelp">
+                                <label for="message">Messaggio</label>
+                                <textarea class="form-control" name="message" required minlength="10" maxlength="700" id="message" cols="30" rows="10"></textarea>
+                            </div>
+                            <input type="hidden" name="house_id" value="{{$house->id}}">
+                            <button class="form__btn " type="submit">Invia</button>
+                        </form>
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div id="map-example-container"></div>
+                    <input type="search" id="input-map" class="form-control"/>
+                    <input id="latitudine" type="hidden"  value=" {{$house->houseinfo->lat}}">
+                    <input id="longitudine" type="hidden"   value=" {{$house->houseinfo->lon}}">
+                    <input id="indirizzo" type="hidden"   value=" {{$house->houseinfo->address}}">
                 </div>
             </div>
         </div>
-    </div>
-    <div id="map-example-container"></div>
-    <input type="search" id="input-map" class="form-control"/>
-    <input id="latitudine" type="hidden"  value=" {{$house->houseinfo->lat}}">
-    <input id="longitudine" type="hidden"   value=" {{$house->houseinfo->lon}}">
-    <input id="indirizzo" type="hidden"   value=" {{$house->houseinfo->address}}">
-
 
     <script src="{{ asset('js/map.js') }}"></script>
 @endsection
