fe438a2bd53cc172c43e0603d78ab8b2365bcf72
Davide Preti <davide.preti86@gmail.com>
2020-12-07 21:52:21 +0100

    piccoli aggiustamenti

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
@@ -27,17 +28,17 @@ class ApiController extends Controller
         $price = $_GET["price"];
         $distance = $_GET["distance"];
         $houses_info = HouseInfo::distance($lat, $lon)
-                                ->having('distance', '<=', $distance)
-                                ->orderBy('distance', 'ASC')
-                                ->where([
-                                    ['rooms', '>=', $rooms],
-                                    ['beds', '>=', $beds],
-                                    ['bathrooms', '>=', $bathrooms],
-                                    ['mq', '>=', $mq],
-                                    ['price', '>=', $price]
-                                ])
-                                ->get();
-        $housesToPrint = [];                            
+            ->having('distance', '<=', $distance)
+            ->orderBy('distance', 'ASC')
+            ->where([
+                ['rooms', '>=', $rooms],
+                ['beds', '>=', $beds],
+                ['bathrooms', '>=', $bathrooms],
+                ['mq', '>=', $mq],
+                ['price', '>=', $price]
+            ])
+            ->get();
+        $housesToPrint = [];
         foreach ($houses_info as $house_info) {
             // Se $services non è vuoto, ciclo sui services per ogni house_info
             if ($services != "") {
@@ -47,97 +48,42 @@ class ApiController extends Controller
                         $tempArray[] = $service;
                     }
                 }
-                if ($tempArray == $services) { 
-                    $housesToPrint[] = $house_info; 
+                if ($tempArray == $services) {
+                    $housesToPrint[] = $house_info;
                 }
-            } 
+            }
             // Se $services è vuoto, passo tutte le houses_info
-            else {       
+            else {
                 $house_info->house;
-                $housesToPrint[] = $house_info; 
+                $housesToPrint[] = $house_info;
             }
         }
         return $housesToPrint;
     }
-    
-    
+
+
     public function statistic()
     {
         $house_id = $_GET["id"];
-        $year = 2019;
+        $year = 2020;
 
         $house = House::where('id', $house_id)->first();
-        // dd($house);
-
-        $stats_array = [
-            1 => [
-                "views" => 0,
-                "messages" => 0
-            ],
-            2 => [
-                "views" => 0,
-                "messages" => 0
-            ],
-            3 => [
-                "views" => 0,
-                "messages" => 0
-            ],
-            4 => [
-                "views" => 0,
-                "messages" => 0
-            ],
-            5 => [
-                "views" => 0,
-                "messages" => 0
-            ],
-            6 => [
-                "views" => 0,
-                "messages" => 0
-            ],
-            7 => [
-                "views" => 0,
-                "messages" => 0
-            ],
-            8 => [
-                "views" => 0,
-                "messages" => 0
-            ],
-            9 => [
-                "views" => 0,
-                "messages" => 0
-            ],
-            10 => [
-                "views" => 0,
-                "messages" => 0
-            ],
-            11 => [
-                "views" => 0,
-                "messages" => 0
-            ],
-            12 => [
-                "views" => 0,
-                "messages" => 0
-            ],
-        ];
 
         $new_arr = [];
 
-
-        foreach ($stats_array as $month => $stat_array) {
+        for ($i = 0; $i <= 12; $i++) {
 
             $views = View::where('house_id', $house_id)
                 ->whereYear('created_at', '=', $year)
-                ->whereMonth('created_at', '=', $month)
+                ->whereMonth('created_at', '=', $i)
                 ->get();
-            $stat_array['views'] = count($views);
 
             $messages = Message::where('house_id', $house_id)
                 ->whereYear('created_at', '=', $year)
-                ->whereMonth('created_at', '=', $month)
+                ->whereMonth('created_at', '=', $i)
                 ->get();
-            $stat_array['messages'] = count($messages);
 
-            $new_arr[$month] = ['views' => $stat_array['views'], 'messages' => $stat_array['messages']];
+            $new_arr[$i] = ['views' => count($views), 'messages' => count($messages)];
         }
 
         return $new_arr;
--- app/Http/Controllers/Host/StatisticController.php
+++ app/Http/Controllers/Host/StatisticController.php
@@ -4,14 +4,22 @@ namespace App\Http\Controllers\Host;
 
 use App\Http\Controllers\Controller;
 
+use Illuminate\Support\Facades\Auth;
+
 use App\House;
-use App\View;
 
 class StatisticController extends Controller
 {
 
     public function index($id)
     {
-        return view("host/statistic.index", compact('id'));
+
+        $house = House::find($id);
+
+        if (Auth::id() == $house->user_id) {
+            return view("host/statistic.index", compact('id'));
+        } else {
+            return redirect()->route('guest/house', $house->slug);
+        }
     }
 }
